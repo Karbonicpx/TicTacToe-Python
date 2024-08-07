c04f235d0d131baaffb3f8eab601001561ec9661
Nicolas Almeida <nicolasalmeida@Nicolass-MacBook-Air.local>
2024-08-06 23:12:23 -0300

    Interface finalizada

---
+++ main.py
@@ -0,0 +1,16 @@
+inputs = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+def interface(grid = 3):
+    
+
+    print(f" {inputs[1]} | {inputs[2]} | {inputs[3]}")
+    print("---|---|---")
+    print(f" {inputs[4]} | {inputs[5]} | {inputs[6]}")
+    print("---|---|---")
+    print(f" {inputs[7]} | {inputs[8]} | {inputs[9]}")
+    print("---|---|---")
+    
+
+print("\n")
+interface()
+print("\n")
